a1f87fceafe661738a78c7a36ab2afffc7debdd5
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-06-01 00:40:16 -0200

    Add files via upload

---
+++ udp.py
@@ -0,0 +1,13 @@
+from socket import *
+server='127.0.0.1'
+port=43210
+obj_socket=socket(AF_INET, SOCK_DGRAM)
+obj_socket.connect((server,port))
+exit=''
+while exit=!'X':
+	msg=input('Insert a message: ')
+	obj_socket.sendto(msg.encode(), (server,port))
+	data, origin = obj_socket.recvfrom(65535)
+	print("Server's answer: ",data.decode())
+	exit=input("Type in 'X' to exit: ").upper()
+obj_socket.close()
